35d91fa1b1351fcdac44ccfd42afedc6545e9d7f
HasinRaihan223 <hraihan20@stuy.edu>
2020-10-28 13:35:20 -0400

    Added setPassword method, deposit, withdrawal methods and all works and tested

--- BankAccount.java
+++ BankAccount.java
@@ -21,4 +21,27 @@ public class BankAccount{
     return accountID;
   }
 
+  public void setPassword(String newPass){
+    password = newPass;
+  }
+
+  public boolean deposit(double amount){
+    if (amount >= 0){
+      balance += amount;
+      return true;
+    }
+    else return false;
+  }
+
+   public boolean withdraw(double amount){
+     if (amount >= 0 && amount < balance){
+       balance -= amount;
+       return true;
+     }
+     else if (balance < amount){
+       return false;
+     }
+     else return false;
+   }
+
 }
--- BankAccountTester.java
+++ BankAccountTester.java
@@ -3,6 +3,15 @@ public class BankAccountTester{
     BankAccount myAccount = new BankAccount(1000001, "pswd");
       System.out.println(myAccount.getBalance());
       System.out.println(myAccount.getAccountID());
+      myAccount.setPassword("new pswd");
+      System.out.println(myAccount.password);
+      System.out.println(myAccount.deposit(1000));
+      System.out.println(myAccount.deposit(-1000));
+      System.out.println(myAccount.getBalance());
+      System.out.println(myAccount.withdraw(-999));
+      System.out.println(myAccount.withdraw(1001));
+      System.out.println(myAccount.withdraw(999));
+      System.out.println(myAccount.getBalance());
       System.out.println(myAccount.toString());
   }
 }
