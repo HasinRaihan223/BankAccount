63e5639da501f350d53c1cc735ca3cf9526b6334
HasinRaihan223 <hraihan20@stuy.edu>
2020-10-28 15:11:05 -0400

    Fixed small nuance

--- BankAccount.java
+++ BankAccount.java
@@ -34,7 +34,7 @@ public class BankAccount{
   }
 
    public boolean withdraw(double amount){
-     if (amount >= 0 && amount < balance){
+     if (amount >= 0 && amount <= balance){
        balance -= amount;
        return true;
      }
