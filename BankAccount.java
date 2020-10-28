c29cae64b49fb868725ac330028f3746ade84a41
HasinRaihan223 <hraihan20@stuy.edu>
2020-10-28 13:44:47 -0400

    Forgt to make varaibles private

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,7 @@
 public class BankAccount{
-  double balance;
-  int accountID;
-  String password;
+  private double balance;
+  private int accountID;
+  private String password;
 
   public BankAccount(int accountID, String password){
     balance = 0;
